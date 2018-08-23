75c1d1417bf1cd0b6c402063ca4160d388fc31a0
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-08-23 01:22:37 -0700

    -changed connection

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,15 @@
 let mysql = require("mysql");
 
 let connection = mysql.createConnection({
-    // port: 3306,
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "cocktails_db",
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    database: "heroku_bfdf8372fc08edc",
-    user: "b211b784302e82",
-    password: "0a74a851"
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "cocktails_db"
+    // host: "us-cdbr-iron-east-01.cleardb.net",
+    // database: "heroku_bfdf8372fc08edc",
+    // user: "b211b784302e82",
+    // password: "0a74a851"
 })
 
 // Make connection
