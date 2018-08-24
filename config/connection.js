6316346fadb7c72516e0a7b9424aaf422ca7b2e2
KKT84 <35786891+kthoma2484@users.noreply.github.com>
2018-08-23 20:16:54 -0700

    -commit

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 // Set up MySQL connection
 let mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-    // port: 3306,
-    // host: "localhost",
-    // user: "root",
-    // password: "",
-    // database: "cocktails_db"
-    host: "us-cdbr-iron-east-01.cleardb.net",
-    database: "heroku_bfdf8372fc08edc",
-    user: "b211b784302e82",
-    password: "0a74a851"
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "cocktails_db"
+    });
+};
 
 // Make connection
 connection.connect(function(err){
